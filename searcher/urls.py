0a5767c517fa0b23cefbd5466c9363cde17a0d40
danicassani <danielduran1995@gmail.com>
2023-09-02 11:04:09 +0200

    initial changes to the views and templates

--- searcher/urls.py
+++ searcher/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("process_images", views.process_images, name="process_images"),
 ]
--- searcher/views.py
+++ searcher/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
     return HttpResponse(render(request, 'searcher/index.html'))
+
+def process_images(request):
+    # TODO: Implement this view
+    return redirect('index')
